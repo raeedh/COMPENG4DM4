967a3380ce53279183c6e6ac0dc1954697d51a48
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2022-10-23 03:20:55 -0400

    update b4

--- Assignment 1/assembly/b4.s
+++ Assignment 1/assembly/b4.s
@@ -12,13 +12,14 @@ XOR		R1,R1,R2	% XOR(b,c)
 ROT.L	R1,R1,#12	% ROTATE_LEFT(b, 12)
 
 ADD		R0,R1,R0	% a = a + b
-SW		(0)Ra,R0	% store a
 XOR		R3,R0,R3	% XOR(d,a)
+SW		(0)Ra,R0	% store a
 ROT.L	R3,R3,#8	% ROTATE_LEFT(d, 8)
-SW		(0)Rd,R3	% store d
 
 ADD		R2,R3,R2	% c = c + d
-SW		(0)Rc,R2	% store c
+SW		(0)Rd,R3	% store d
 XOR		R1,R1,R2	% XOR(b,c)
 ROT.L	R1,R1,#7	% ROTATE_LEFT(b, 7)
+
+SW		(0)Rc,R2	% store c
 SW		(0)Rb,R1	% store b
