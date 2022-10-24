adae74dab527640eb51b5ae468c3bd3c9930e5c9
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2022-10-23 22:00:01 -0400

    update b1,b2 with macros

--- Assignment 1/assembly/b1.s
+++ Assignment 1/assembly/b1.s
@@ -1,13 +1,20 @@
-LW		R0,(0)Ra	% load a
-LW		R1,(0)Rb	% load b
-ADD		R0,R1,R0	% a = a + b
-SW		(0)Ra,R0	% store a
+% ASSUMPTION 1: assume that register R0 contains the address of the first word of the block of the initial key-stream
+% ASSUMPTION 2: each word is 32 bits, so use LD instead of LW to load 32-bit words
+% ASSUMPTION 3: words in the initial key-stream are stored consecutively in memory, address of second word is address of first world + 4 (bytes))
 
-LW		R0,(0)Ra	% load a
-LW		R2,(0)Rd	% load d
-XOR		R2,R0,R2	% XOR(d,a)
-SW		(0)Rd,R2	% store d
+% macros
+#define Ra => R1; Rb => R2; Rd => R3	% define Ra, Rb, Rd
 
-LW		R2,(0)Rd	% load d	
-ROT.L	R2,R2,#16	% ROTATE_LEFT(d, 16)
-SW		(0)Rd,R2	% store d
+LD	Ra,(0)R0	% load a
+LD	Rb,(16)R0	% load b
+ADD	Ra,Ra,Rb	% a = a + b
+SW	(0)R0,Ra	% store a
+
+LD	Ra,(0)R0	% load a
+LD	Rd,(48)R0	% load d
+XOR	Rd,Rd,Rd	% XOR(d,a)
+SW	(48)R0,Rd	% store d
+
+LD	Rd,(48)R0	% load d	
+ROT.L	Rd,Rd,#16	% ROTATE_LEFT(d, 16)
+SW	(48)R0,Rd	% store d
--- Assignment 1/assembly/b2.s
+++ Assignment 1/assembly/b2.s
@@ -1,59 +1,64 @@
+% ASSUMPTION 1: assume that register R0 contains the address of the first word of the block of the initial key-stream
+% ASSUMPTION 2: each word is 32 bits, so use LD instead of LW to load 32-bit words
+
+#define Ra => R1; Rb => R2; ; Rc => R3; Rd => R4	% define Ra, Rb, Rc, Rd
+
 % lines 1-3
-LW	R0,(0)Ra	% load a
-LW	R1,(0)Rb	% load b
-ADD	R0,R1,R0	% a = a + b
-SW	(0)Ra,R0	% store a
+LD	Ra,(0)R0	% load a
+LD	Rb,(16)R0	% load b
+ADD	Ra,Ra,Rb	% a = a + b
+SW	(0)R0,Ra	% store a
 
-LW	R0,(0)Ra	% load a
-LW	R3,(0)Rd	% load d
-XOR	R3,R0,R3	% XOR(d,a)
-SW	(0)Rd,R3	% store d
+LD	Ra,(0)R0	% load a
+LD	Rd,(48)R0	% load d
+XOR	Rd,Rd,Rd	% XOR(d,a)
+SW	(48)R0,Rd	% store d
 
-LW	R3,(0)Rd	% load d	
-ROT.L	R3,R3,#16	% ROTATE_LEFT(d, 16)
-SW	(0)Rd,R3	% store d
+LD	Rd,(48)R0	% load d	
+ROT.L	Rd,Rd,#16	% ROTATE_LEFT(d, 16)
+SW	(48)R0,Rd	% store d
 
 % lines 4-6
-LW	R2,(0)Rc	% load c
-LW	R3,(0)Rd	% load d
-ADD	R2,R3,R2	% c = c + d
-SW	(0)Rc,R2	% store c
+LW	Rc,(32)R0	% load c
+LW	Rd,(48)R0	% load d
+ADD	Rc,Rc,Rd	% c = c + d
+SW	(32)R0,Rc	% store c
 
-LW	R1,(0)Rb	% load b
-LW	R2,(0)Rc	% load c
-XOR	R1,R1,R2	% XOR(b,c)
-SW	(0)Rb,R1	% store b
+LW	Rb,(16)R0	% load b
+LW	Rc,(32)R0	% load c
+XOR	Rb,Rb,Rc	% XOR(b,c)
+SW	(16)R0,Rb	% store b
 
-LW	R1,(0)Rb	% load b	
-ROT.L	R1,R1,#12	% ROTATE_LEFT(b, 12)
-SW	(0)Rb,R1	% store b
+LW	Rb,(16)R0	% load b	
+ROT.L	Rb,Rb,#12	% ROTATE_LEFT(b, 12)
+SW	(16)R0,Rb	% store b
 
 % lines 7-9
-LW	R0,(0)Ra	% load a
-LW	R1,(0)Rb	% load b
-ADD	R0,R1,R0	% a = a + b
-SW	(0)Ra,R0	% store a
+LD	Ra,(0)R0	% load a
+LD	Rb,(16)R0	% load b
+ADD	Ra,Ra,Rb	% a = a + b
+SW	(0)R0,Ra	% store a
 
-LW	R0,(0)Ra	% load a
-LW	R3,(0)Rd	% load d
-XOR	R3,R0,R3	% XOR(d,a)
-SW	(0)Rd,R3	% store d
+LD	Ra,(0)R0	% load a
+LD	Rd,(48)R0	% load d
+XOR	Rd,Rd,Rd	% XOR(d,a)
+SW	(48)R0,Rd	% store d
 
-LW	R3,(0)Rd	% load d	
-ROT.L	R3,R3,#8	% ROTATE_LEFT(d, 8)
-SW	(0)Rd,R3	% store d
+LD	Rd,(48)R0	% load d	
+ROT.L	Rd,Rd,#8	% ROTATE_LEFT(d, 8)
+SW	(48)R0,Rd	% store d
 
 % lines 10-12
-LW	R2,(0)Rc	% load c
-LW	R3,(0)Rd	% load d
-ADD	R2,R3,R2	% c = c + d
-SW	(0)Rc,R2	% store c
+LW	Rc,(32)R0	% load c
+LW	Rd,(48)R0	% load d
+ADD	Rc,Rc,Rd	% c = c + d
+SW	(32)R0,Rc	% store c
 
-LW	R1,(0)Rb	% load b
-LW	R2,(0)Rc	% load c
-XOR	R1,R1,R2	% XOR(b,c)
-SW	(0)Rb,R1	% store b
+LW	Rb,(16)R0	% load b
+LW	Rc,(32)R0	% load c
+XOR	Rb,Rb,Rc	% XOR(b,c)
+SW	(16)R0,Rb	% store b
 
-LW	R1,(0)Rb	% load b	
-ROT.L	R1,R1,#7	% ROTATE_LEFT(b, 7)
-SW	(0)Rb,R1	% store b
+LW	Rb,(16)R0	% load b	
+ROT.L	Rb,Rb,#7	% ROTATE_LEFT(b, 7)
+SW	(16)R0,Rb	% store b
