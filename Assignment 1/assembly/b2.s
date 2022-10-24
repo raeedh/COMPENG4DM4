c899edae5d14a82a19f7d45a5201b5d18722a9a4
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2022-10-24 16:03:01 -0400

    fix LWs

--- Assignment 1/assembly/b2.s
+++ Assignment 1/assembly/b2.s
@@ -1,5 +1,5 @@
 % ASSUMPTION 1: assume that register R0 contains the address of the first word of the block of the initial key-stream
-% ASSUMPTION 2: each word is 32 bits, so use LD and SD instead of LW and SD to load 32-bit words
+% ASSUMPTION 2: each word is 32 bits, so use LD and SD instead of LD and SD to load 32-bit words
 % ASSUMPTION 3: words in the initial key-stream are stored consecutively in memory, address of second word is address of first world + 4 (bytes)
 % ASSUMPTION 4: assume dual-ported memory that allows simultaneous read+read, read+write, write+write
 
@@ -21,17 +21,17 @@ ROT.L	Rd,Rd,#16	% ROTATE_LEFT(d, 16)
 SD	(48)R0,Rd	% store d in memory
 
 % lines 4-6
-LW	Rc,(32)R0	% load c from memory
-LW	Rd,(48)R0	% load d from memory
+LD	Rc,(32)R0	% load c from memory
+LD	Rd,(48)R0	% load d from memory
 ADD	Rc,Rc,Rd	% c = c + d
 SD	(32)R0,Rc	% store c in memory
 
-LW	Rb,(16)R0	% load b from memory
-LW	Rc,(32)R0	% load c from memory
+LD	Rb,(16)R0	% load b from memory
+LD	Rc,(32)R0	% load c from memory
 XOR	Rb,Rb,Rc	% XOR(b,c)
 SD	(16)R0,Rb	% store b in memory
 
-LW	Rb,(16)R0	% load b from memory	
+LD	Rb,(16)R0	% load b from memory	
 ROT.L	Rb,Rb,#12	% ROTATE_LEFT(b, 12)
 SD	(16)R0,Rb	% store b in memory
 
@@ -51,16 +51,16 @@ ROT.L	Rd,Rd,#8	% ROTATE_LEFT(d, 8)
 SD	(48)R0,Rd	% store d in memory
 
 % lines 10-12
-LW	Rc,(32)R0	% load c from memory
-LW	Rd,(48)R0	% load d from memory
+LD	Rc,(32)R0	% load c from memory
+LD	Rd,(48)R0	% load d from memory
 ADD	Rc,Rc,Rd	% c = c + d
 SD	(32)R0,Rc	% store c in memory
 
-LW	Rb,(16)R0	% load b from memory
-LW	Rc,(32)R0	% load c from memory
+LD	Rb,(16)R0	% load b from memory
+LD	Rc,(32)R0	% load c from memory
 XOR	Rb,Rb,Rc	% XOR(b,c)
 SD	(16)R0,Rb	% store b in memory
 
-LW	Rb,(16)R0	% load b from memory	
+LD	Rb,(16)R0	% load b from memory	
 ROT.L	Rb,Rb,#7	% ROTATE_LEFT(b, 7)
 SD	(16)R0,Rb	% store b in memory
